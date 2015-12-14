eb1e77207488b6be802f73de51c4033d7d6fde86
Matthew Zimmerman <matt.zimmerman@ccri.com>
2015-12-14 17:01:29 -0500

    Initial commit.

---
+++ 1/day1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+file="input.txt"
+
+n=1
+floor=0
+fh=open(file, 'r')
+for line in fh:
+    for c in list(line):
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+        if floor == -1:
+            print "%d %s %3d" % (n, c, floor)
+        n += 1
+
+
