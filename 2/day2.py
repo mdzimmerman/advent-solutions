34b57686f99a1383d053a17e68da0c9f28b6f9b0
Matthew Zimmerman <matt.zimmerman@ccri.com>
2015-12-14 17:02:16 -0500

    Implementation of first part of day 2.

---
+++ 2/day2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+input="input.txt"
+
+fh=open(input,"r")
+total=0
+for line in fh:
+    line=line.rstrip()
+    #print line
+    (x, y, z) = [int(i) for i in line.split("x")]
+    area = [x*y, y*z, x*z]
+    minarea = min(area)
+    totalarea = 2*area[0] + 2*area[1] + 2*area[2] + minarea
+    total += totalarea
+    print "dim: %2d %2d %2d areas: %4d %4d %4d minarea: %4d totalarea: %5d TOTAL: %7d" % (x, y, z, area[0], area[1], area[2], minarea, totalarea, total)
